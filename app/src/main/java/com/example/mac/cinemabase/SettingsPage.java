15e8a5308ba478076d943eafe045feda7dab60f2
Danny Hernandez <dehernandez7791@gmail.com>
2015-04-17 01:03:31 -0700

    settings page created

--- app/src/main/java/com/example/mac/cinemabase/SettingsPage.java
+++ app/src/main/java/com/example/mac/cinemabase/SettingsPage.java
@@ -1,27 +1,24 @@
 package com.example.mac.cinemabase;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class SettingsPage extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings_page);
     }
 
 
-    @Override
+/*    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_settings_page, menu);
         return true;
     }
 
@@ -34,13 +31,11 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            /*Toast toast = Toast.makeText(getApplicationContext(),"settings menu",Toast.LENGTH_LONG);
-            toast.show();*/
-            Intent intent = new Intent(this, SettingsPage.class);
-            startActivity(intent);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
+
+
 }
